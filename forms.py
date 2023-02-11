87527ccd37f2281f838ab7f5e5356e06c50852af
Kle012 <kimhle.0102@gmail.com>
2023-02-10 18:00:07 -0800

    Modifed UserAddForm

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, TextAreaField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, Optional
 
 
 class MessageForm(FlaskForm):
@@ -15,7 +15,11 @@ class UserAddForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[Length(min=6)])
-    image_url = StringField('(Optional) Image URL')
+    location = StringField('Location', validators=[Optional()])
+    bio = TextAreaField('Bio', validators=[Optional()])
+    image_url = StringField('(Optional) Image URL', validators=[Optional()])
+    header_image_url = StringField('(Optional) Header_Image URL', validators=[Optional()])
+
 
 
 class LoginForm(FlaskForm):
