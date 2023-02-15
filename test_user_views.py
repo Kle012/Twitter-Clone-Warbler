f5340ea71427bcbd5c89fcda21dfca8bb42f8999
Kle012 <kimhle.0102@gmail.com>
2023-02-14 17:11:53 -0800

    Test views for users

---
+++ test_user_views.py
@@ -0,0 +1,32 @@
+"""User view tests."""
+
+# run these tests like:
+#
+# FLASK_ENV=production python -m unittest test_user_views.py
+
+import os
+from unittest import TestCase
+from sqlalchemy import exc
+
+from models import db, connect_db,User, Message, Follows, Likes
+
+os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
+
+from app import app
+
+db.create_all()
+
+app.config['WTF_CSRF_ENABLED'] = False
+
+class MessageViewTestCase(TestCase):
+    """Test views for messages."""
+
+    def setUp(self):
+        """Create test client, add sample data."""
+        db.drop_all()
+        db.create_all()
+
+        self.client = app.test_client()
+        
+
+
